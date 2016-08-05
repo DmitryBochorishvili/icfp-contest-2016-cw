8189b11efc876118dedacb4824efa60ecd7e1e9a
Victor Sergienko <singalen@gmail.com>
2016-08-06 02:22:01 +0300

    ops

--- src/main/java/cw/icfpc/model/DecisionTree.java
+++ src/main/java/cw/icfpc/model/DecisionTree.java
@@ -1,27 +1,32 @@
 package cw.icfpc.model;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class DecisionTree
 {
-    public void generateDecisionNodes(State state)
+    public static List<State> generateDecisionNodes(State state)
     {
-
-        AdjacentPolyGenerator polyGenerator = new AdjacentPolyGenerator();
+        List<State> nodes = new LinkedList<>();
 
         state.getEdges().forEach(edge -> {
             Iterable<CompoundPolygon> compounds = AdjacentPolyGenerator.getAllCompounds(state, edge);
             compounds.forEach(cp -> {
-//                CompoundPolygon flippedCompound = cp.flip(edge);
+                CompoundPolygon flippedCompound = cp.flip(edge);
+                State newState = state.addCompound(flippedCompound);
+                nodes.add(newState);
+                
 //                cp.getSubsets().forEach(compoundSubset -> {
 
                     // из начального состояния вычесть compound
                     // добавить flippedCompound в его возмножных конфигурациях
                     // добавить сабсет в его возможных конфигурациях
 
-//                        State newState =
-//                        compoundSubset
+//                        State newState = compoundSubset
 //                });
             });
         });
 
+        return nodes;
     }
 }
