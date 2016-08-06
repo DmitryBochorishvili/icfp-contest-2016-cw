573a6ffcace05897578bc1c5235b58654e4f26b0
sanyok-ua <alexander.ogol@gmail.com>
2016-08-06 21:25:41 +0300

    submit solutions API call

--- src/main/java/cw/icfpc/ServerCommunicator.java
+++ src/main/java/cw/icfpc/ServerCommunicator.java
@@ -1,15 +1,22 @@
 package cw.icfpc;
 
 import com.google.gson.Gson;
+import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
 
 import java.io.*;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.apache.http.protocol.HTTP.USER_AGENT;
 
@@ -175,7 +182,21 @@ public class ServerCommunicator {
         return result;
     }
 
+    public static String submitSolution(String taskId, String solution) {
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("problem_id", taskId));
+        params.add(new BasicNameValuePair("solution_spec", solution));
+
+        String result = callAPIMethod("solution/submit", params);
+        System.out.print(result);
+        return result;
+    }
+
     private static String callAPIMethod(String apiCall) {
+        return callAPIMethod(apiCall, null);
+    }
+
+    private static String callAPIMethod(String apiCall, List<NameValuePair> params) {
         try {
             Thread.sleep(requestTimeOut);
         } catch (InterruptedException e) {
@@ -185,25 +206,34 @@ public class ServerCommunicator {
         String url = "http://2016sv.icfpcontest.org/api/" + apiCall;
 
         HttpClient client = HttpClientBuilder.create().build();
-        HttpGet request = new HttpGet(url);
 
-// add request header
-        request.addHeader("X-API-Key", teamApiKey);
-        HttpResponse response = null;
         try {
+            HttpUriRequest request = null;
+            if(params == null || params.size() == 0) { // GET
+                request = new HttpGet(url);
+            }
+            else {
+                HttpPost post = new HttpPost(url);
+                post.setEntity(new UrlEncodedFormEntity(params));
+                request = post;
+            }
+
+            // add request header
+            request.addHeader("X-API-Key", teamApiKey);
+            HttpResponse response = null;
             response = client.execute(request);
 
-        System.out.println("Response Code : "
-                + response.getStatusLine().getStatusCode());
+            System.out.println("Response Code : "
+                    + response.getStatusLine().getStatusCode());
 
-        BufferedReader rd = new BufferedReader(
-                new InputStreamReader(response.getEntity().getContent()));
+            BufferedReader rd = new BufferedReader(
+                    new InputStreamReader(response.getEntity().getContent()));
 
-        StringBuffer result = new StringBuffer();
-        String line = "";
-        while ((line = rd.readLine()) != null) {
-            result.append(line);
-            result.append(System.getProperty("line.separator"));
+            StringBuffer result = new StringBuffer();
+            String line = "";
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+                result.append(System.getProperty("line.separator"));
         }
         return result.toString();
 
